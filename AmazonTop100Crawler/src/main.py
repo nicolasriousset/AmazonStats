365859de8085c4ba6fa3ccafb7d86053effe530e
nicolas@riousset.com <nicolas@riousset.com>
2014-07-05 22:52:50 +0200

    The crawler now saves the Amazon Top 100 html files locally. A CSV file is generated with the top 100 summary.

--- AmazonTop100Crawler/src/main.py
+++ AmazonTop100Crawler/src/main.py
@@ -1,11 +1,14 @@
+# -*- coding: utf-8 -*-
 import urllib2
 import re
 import os
+import datetime
+import codecs
 from bs4 import BeautifulSoup
 
-def downloadAd(adUrl, destinationFolder):
-    print(adUrl)
-    fields = adUrl.split("-")
+def downloadTop100(top100Url, destinationFolder):
+    print(top100Url)
+    fields = top100Url.split("-")
     filename = os.path.join(destinationFolder, fields[-1] + ".html")
     
     if os.path.exists(filename):
@@ -24,10 +27,8 @@ def downloadAd(adUrl, destinationFolder):
     except urllib2.URLError as e:
         print("Failed to download from " + adUrl + ":" + e.reason)
 
-def getEbookCategories(ebookUrl):
+def parseEbookCategories(html):
     try:
-        response = urllib2.urlopen(ebookUrl)
-        html = response.read()
 
         # Must use the lxml HTML parser even though it has external dependecies, because the default python one is not good enough to parse the Kijiji pages
         # lxml Windows distribution was downloaded from http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
@@ -45,10 +46,21 @@ def getEbookCategories(ebookUrl):
         print("Failed to download from " + ebookUrl + ":" + e.reason)
         
                 
-def getTop100Info(resultsPageUrl, destinationFolder):
+def getTop100Info(resultsPageUrl, pageIndex, destinationFolder, csvFile):
     try:
-        response = urllib2.urlopen(resultsPageUrl)
+        response = urllib2.urlopen(resultsPageUrl + str(pageIndex))
         html = response.read()
+        
+        fromRank = (pageIndex-1) * 20 + 1
+        toRank = fromRank + 19 
+        localFile = open(os.path.join(destinationFolder, str(fromRank) + "-" + str(toRank) + ".html"), "w")
+        localFile.write(html)
+        localFile.close()
+        
+        ebooksDestinationFolder = os.path.join(destinationFolder, "ebooks")
+        if not os.path.exists(ebooksDestinationFolder): 
+            os.makedirs(ebooksDestinationFolder)
+
         
         # Must use the lxml HTML parser even though it has external dependecies, because the default python one is not good enough to parse the Kijiji pages
         # lxml Windows distribution was downloaded from http://www.lfd.uci.edu/~gohlke/pythonlibs/#lxml
@@ -73,24 +85,40 @@ def getTop100Info(resultsPageUrl, destinationFolder):
                     rating = float(avgStarsTag.text.split(' ', 1)[0])
                     nbReviews = int(avgStarsTag.find_all('a')[2].text)
             price = float(itemWrapper.find('div', {'class', 'zg_itemPriceBlock_compact'}).text.split(' ')[2].replace(',', '.'))
-            categories = getEbookCategories(ebookUrl)
-            print(str(rank)  + '\t' + str(daysInTop100) + '\t' + str(rating) + '\t' + str(nbReviews) + '\t' + str(price) + '\t' + title + '\t' + asin + '\t' + author + '\t' + ebookUrl + '\t' + categories)
+            response = urllib2.urlopen(ebookUrl)
+            ebookHtml = response.read()
+            ebookFileName = os.path.join(ebooksDestinationFolder, asin + ".html")
+            ebookFile = open(ebookFileName, "w")
+            ebookFile.write(ebookHtml)
+            ebookFile.close()
+
+            categories = parseEbookCategories(ebookHtml)
+            line = str(rank)  + '\t' + str(daysInTop100) + '\t' + str(rating) + '\t' + str(nbReviews) + '\t' + str(price) + '\t' + title + '\t' + asin + '\t' + author + '\t' + ebookUrl + '\t' + categories
+            print(line)
+            csvFile.write(line + "\r\n")
+            csvFile.flush()
             
     except urllib2.URLError as e:
         print("Failed to download from " + resultsPageUrl + ":" + e.reason)
     
 def main():
-    destinationFolder = "data"
+    destinationFolder = os.path.join("data", str(datetime.date.today()) + " " + str(datetime.datetime.now().strftime("%Hh%M")))
     if not os.path.exists(destinationFolder): 
         os.makedirs(destinationFolder)
 
-    print('Rank\tDaysInTop100\tRating\tNbReviews\tPrice\tTitle\tASIN\tAuthor\tURL')
+    csvFileName = os.path.join(destinationFolder, "top100.csv")
+    
+    csvFile = codecs.open(csvFileName, "w", "latin-1")
+    print("Rank\tDaysInTop100\tRating\tNbReviews\tPrice\tTitle\tASIN\tAuthor\tURL")
+    csvFile.write("Rank\tDaysInTop100\tRating\tNbReviews\tPrice\tTitle\tASIN\tAuthor\tURL\r\n")
     
     #bestSellersPage = 'http://www.amazon.fr/gp/bestsellers/digital-text/695398031#3';
 
     bestSellersPage = 'http://www.amazon.fr/gp/bestsellers/digital-text/695398031/ref=zg_bs_695398031_pg_2/276-1310913-3876205?ie=UTF8&pg='
     for i in range(1, 6):
-        getTop100Info(bestSellersPage + str(i), destinationFolder)
+        getTop100Info(bestSellersPage, i, destinationFolder, csvFile)
+        
+    csvFile.close()
     
 if __name__ == "__main__":
     main()
